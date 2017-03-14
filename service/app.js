46da20c8e84bc06edbc7d638e57a8e41e4028efb
kuvshinov <kuvshinov@users.noreply.github.com>
2017-03-14 15:43:12 +0300

    remove unnecessary files

--- service/app.js
+++ service/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +42,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.VK_APP_ID = '';
 
 module.exports = app;
--- service/routes/index.js
+++ service/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log(req.cookies);
+	if (checkAuth(req.cookies['vk_app_' + req.app.VK_APP_ID])) {
+		res.render('audio');
+	} else {
+  	res.render('index', { title: 'Express' });
+	}
 });
+
+function checkAuth(cookieString) {
+	return false;
+}
 
 module.exports = router;
